557fec18b96e1efd277932e0c3ca7bde71c1c0fa
LJ S.A <lvjhn.d@gmail.com>
2025-02-14 23:58:18 +0800

    general update

--- .mods/backend/models/helpers/create-polymorphic-models.ts
+++ .mods/backend/models/helpers/create-polymorphic-models.ts
@@ -1,25 +1,35 @@
 import { belongsTo } from "@adonisjs/lucid/orm"
 
-function createClass(table: any, Something: any, ReferredClass: any) {
+function createClass(
+    table: any, 
+    Something: any, 
+    ReferredClass: any, 
+    foreignKey : string
+) {
 
     class DynamicClass extends Something {
         static table = table;
 
-        @belongsTo(ReferredClass)
+        @belongsTo(ReferredClass, { foreignKey })
         declare liked : any;
     }
 
     return DynamicClass;
 }
 
-export function createPolymorphicModels(mappings: any, Something: any) {
+export function createPolymorphicModels(
+    PolymorphicModel : any,
+    Something: any, 
+) {
     const classes: any = {};
+    const mappings = PolymorphicModel.mappings()
+    const foreignKey = PolymorphicModel.foreignKey
 
     for (let likeable in mappings) {
         const model = likeable;
         const table = mappings[model][0];
         const ReferredClass = mappings[model][1];
-        classes[model] = createClass(table, Something, ReferredClass);
+        classes[model] = createClass(table, Something, ReferredClass, foreignKey);
     }
 
     return classes;
